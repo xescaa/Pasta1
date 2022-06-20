478bd77d4dc2184a02c1a86fdc1cc97cb3731699
FRANCISCO ANGHINONI <102160768+xescaa@users.noreply.github.com>
2022-06-20 14:37:01 -0400

    tarde

--- Kenzie/Arquivos/Java Script/Logica.js
+++ Kenzie/Arquivos/Java Script/Logica.js
@@ -1,4 +1,45 @@
-let preco = 15
-let precoFormatado = "R$15,00"
+// function somandoDoisValores (valor1, valor2){
 
-console.log(`O valor  ${preco}  formatado ficaria assim:  ${precoFormatado}`)
+//     return valor1 + valor2
+
+// }
+// console.log (somandoDoisValores(15, 876))
+
+
+
+
+
+// function avaliarTipo (tipo){
+//     return typeof(tipo)
+// }
+// console.log(avaliarTipo(false))
+
+
+
+
+// function somarNota (nota1, nota2, nota3, nota4){
+
+//     if (typeof(nota1, nota2, nota3, nota4) === "number") {
+//         return nota1 + nota2 + nota3 + nota4
+//     } else {
+//         return "A função só poderá receber números"
+//     }
+
+// }
+// console.log(somarNota(4, 7, 4, 8))
+
+
+
+function soma(valor1, valor2, valor3, valor4){
+    return valor1 + valor2 + valor3 + valor4
+  }
+
+function media(value){
+    return value / 4   
+}
+
+function resultado(nota1, nota2, nota3, nota4){ 
+    return media(soma(nota1, nota2, nota3, nota4))
+  }
+
+console.log('Média do aluno Carlos: ' + resultado(5, 6, 8, 9))
