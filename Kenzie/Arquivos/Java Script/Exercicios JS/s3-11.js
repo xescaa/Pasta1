d9c1f6111b387c4c62df866b403e1e6546eacbd1
FRANCISCO ANGHINONI <102160768+xescaa@users.noreply.github.com>
2022-06-20 07:51:59 -0400

    zdv

---
+++ Kenzie/Arquivos/Java Script/Exercicios JS/s3-11.js
@@ -0,0 +1,6 @@
+let nome = "francisco"
+let sobrenome = "anghinoni"
+let anoNascimento = 1991
+let cidade = "cascavel"
+
+console.log(`Eu sou ${nome} tenho ${2022 - anoNascimento} anos e moro em ${cidade}`)
---
+++ Kenzie/Arquivos/Java Script/Logica.js
@@ -0,0 +1,4 @@
+let preco = 15
+let precoFormatado = "R$15,00"
+
+console.log(`O valor  ${preco}  formatado ficaria assim:  ${precoFormatado}`)
