8e1d9374818b244ac9b88a8c101474e7eb62e35d
FRANCISCO ANGHINONI <102160768+xescaa@users.noreply.github.com>
2022-06-21 14:40:14 -0400

    aasas

---
+++ Kenzie/Arquivos/Java Script/Arrays/S4-08/Script.js
@@ -0,0 +1,10 @@
+let minhaPrimeiraLista = [
+    "Matemática",
+    "Química",
+    "Geografia",
+    "Português",
+    "Física",
+  ];
+  console.log(minhaPrimeiraLista[2])
+  minhaPrimeiraLista[2] = "História";
+  console.log(minhaPrimeiraLista);
