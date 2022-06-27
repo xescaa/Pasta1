33ebc32babe491373a48f2252e64bb6ee8ef7fd7
FRANCISCO ANGHINONI <102160768+xescaa@users.noreply.github.com>
2022-06-27 18:37:15 -0400

    asfa

---
+++ Kenzie/Arquivos/Java Script/Exercicios JS/S05-04.js
@@ -0,0 +1,14 @@
+function numNegativo ([]){
+let qtdNeg = 0
+    for (let i = 0; i <= numNegativo.length ; i++){
+        if (i < 0){
+            return qtdNeg = qtdNeg + 1
+            
+        } else {}
+        
+        
+    }
+    
+
+}
+console.log(numNegativo([-82,-41,44,-94,90,76,62,-33,62,58]))
