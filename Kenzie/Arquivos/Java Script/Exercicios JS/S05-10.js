6b7eeabed6b5a0ae64588c588ad13ff66bcf5295
FRANCISCO ANGHINONI <102160768+xescaa@users.noreply.github.com>
2022-06-30 16:17:17 -0400

    ewrh

--- Kenzie/Arquivos/Java Script/Exercicios JS/S05-10.js
+++ Kenzie/Arquivos/Java Script/Exercicios JS/S05-10.js
@@ -1,35 +1,55 @@
-// Crie uma função que leia uma string neste exemplo usamos:Pneumoultramicroscopicossilicovulcanoconiótico
-// Crie a lógica para deixar todas as letras em maiúsculo.
-// Crie a lógica para substituir as letras 'o' por 'y'.
-// Crie a lógica para remover as letras repetidas.
-// Crie a lógica para inverter a palavra resultante.
-// Crie a lógica para deixar somente a letra inicial maiúsculo.
 
-// Crie uma função que leia uma string neste exemplo usamos:Pneumoultramicroscopicossilicovulcanoconiótico
 function lerString(string){
 
 
    
+// // Crie a lógica para deixar todas as letras em maiúsculo.
+let stringMaiuscula = string.toUpperCase()  
+    console.log(stringMaiuscula)
 
-// Crie a lógica para deixar todas as letras em maiúsculo.
-    // let stringMaiuscula = string.toUpperCase()  
-//     return stringMaiuscula
+
+// // Crie a lógica para substituir as letras 'o' por 'y'.
+let oPorY = string.replace(/o/gi, 'y') 
+    console.log(oPorY)
 
 
-// Crie a lógica para substituir as letras 'o' por 'y'.
-
-    // console.log(string.replace(/o/gi, 'y') )
 
 // Crie a lógica para remover as letras repetidas.
     
-let novaString = ""
+let novaString = " "
+let contador = 0
+    
+for (let i = 0; i < string.length; i++){
+        
+    if (novaString.includes(string[i])){
 
-    for (let i = 0; i <= string.legth; i++){
-        if ()
+    }else {novaString+=string[i]}
+        
+    }
+console.log(novaString)
+
+// Crie a lógica para inverter a palavra resultante.
+
+let palavraInvertida = ""
+    
+for (let i = novaString.length-1; i > 0; i--){
+        palavraInvertida+=novaString[i]
+    }
+
+console.log(palavraInvertida)
 
 
-       }
+// Crie a lógica para deixar somente a letra inicial maiúsculo.
 
+let primeiraMaiuscula = ""
+    for (i = 0; i <= palavraInvertida.length-1; i++){
+        if (i == 0){
+            primeiraMaiuscula+= palavraInvertida[i].toUpperCase()
+        }else{
+            primeiraMaiuscula+= palavraInvertida[i]
+        }
+    }
+console.log(primeiraMaiuscula)
 
 }
 console.log(lerString("Pneumoultramicroscopicossilicovulcanoconiotico"))
