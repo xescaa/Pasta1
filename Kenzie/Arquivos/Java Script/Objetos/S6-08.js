bfab9862793969709112c4dc684427f9ea9e61f7
FRANCISCO ANGHINONI <102160768+xescaa@users.noreply.github.com>
2022-07-06 16:34:49 -0400

    final

--- Kenzie/Arquivos/Java Script/Objetos/S6-08.js
+++ Kenzie/Arquivos/Java Script/Objetos/S6-08.js
@@ -22,9 +22,58 @@ function normalizaDado(){
 
 
 function criandoListaDeUsuarios(pessoa){
-  let listaDeUsuarios = []
-  let nomeDeUsuario = ""   
-
-    listaDeUsuarios.push[pessoa]
+  let listaDeUsuarios = [] 
+  let objetoTratado = pessoa.nomeDeUsuario.toLowerCase()
+  pessoa.nomeDeUsuario = objetoTratado
+  listaDeUsuarios.push(pessoa)
+  
     console.log(listaDeUsuarios)
 }
+criandoListaDeUsuarios(pessoa)
+
+
+function calculaVale(pessoa){
+  let valorDoLitro = 6.99
+  //CRIAR VARIAVEL PARA O VALE COMBUSTIVEL?
+  // let calcValeCombustivel = pessoa.valeCombustivel
+  if (pessoa.temCarro == true){
+    let calcValeCombustivel = parseInt(pessoa.distanciaDoTrabalho) * valorDoLitro * 0.7
+   
+    pessoa.valeCombustivel = calcValeCombustivel
+    
+    
+    // console.log(calcValeCombustivel)
+    
+  }
+  let transf = pessoa.valeCombustivel.toString()
+ 
+  console.log("R$" + transf )
+  
+  }
+  calculaVale(pessoa)
+
+let endereco = {
+  cidade: 'Curitiba',
+  logradouro: 'Rua Dali',
+  numero: 10,
+  tipo:[]
+  }
+  
+
+function adicionarTipo(endereco){
+  let varcasa = "Casa"
+  endereco.tipo.push(varcasa)
+    console.log(endereco.tipo)
+
+  }
+  adicionarTipo(endereco)
+
+
+  function inserirPessoa(endereco, pessoa){
+
+    pessoa.endereco.push(endereco)
+    console.log(pessoa)
+
+      
+  }
+  inserirPessoa(endereco, pessoa)
