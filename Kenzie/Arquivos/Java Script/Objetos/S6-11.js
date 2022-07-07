c06a2ff5a274c7d4e248ca9e3aab41dee5d62e8e
FRANCISCO ANGHINONI <102160768+xescaa@users.noreply.github.com>
2022-07-07 18:13:41 -0400

    dfh

--- Kenzie/Arquivos/Java Script/Objetos/S6-11.js
+++ Kenzie/Arquivos/Java Script/Objetos/S6-11.js
@@ -13,7 +13,33 @@ let comissao5 = 0
 
         }
     
-
-    
 }
 calculateSalary(1200 , 1200)
+
+
+function cashMachine(saque, fixo, vendas){
+
+    let salarioTotal = calculateSalary(fixo, vendas)
+    
+
+    if (salarioTotal < saque){
+        return "sem dinheiros"
+    }
+    let saldoAtual = salarioTotal - saque
+    let nota200 = saque / 200
+    if(saque % 200 === 0 ){
+        return (
+        `Notas sacadas: ${nota200} notas de R$200 
+         Saldo Atual: ${saldoAtual}`)
+    }
+    
+    let nota100 = 0
+    let nota50 = 0
+    let nota20 = 0
+    let nota10 = 0
+    let nota5 = 0
+    let nota2 = 0
+    
+
+
+}
