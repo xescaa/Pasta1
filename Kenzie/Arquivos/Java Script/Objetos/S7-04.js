b9051b3d7dbd21cff4a4beed45f846cac3341d18
FRANCISCO ANGHINONI <102160768+xescaa@users.noreply.github.com>
2022-07-12 15:50:14 -0400

    df

--- Kenzie/Arquivos/Java Script/Objetos/S7-04.js
+++ Kenzie/Arquivos/Java Script/Objetos/S7-04.js
@@ -7,25 +7,49 @@ let produto = {
 
 const tagUl = document.querySelector(".lista")
 
+const tagLi = document.createElement("li")
+    tagLi.classList.add("li")
 
-    const tagLi = document.createElement("li")
-    const nome = document.createElement("span")
+const nome = document.createElement("span")
     nome.innerText = produto.nome
-    const valor = document.createElement("span")
+
+const valor = document.createElement("span")
     valor.innerText = produto.valor
-    const liquidacao = document.createElement("a")
-    liquidacao.innerText = "Em liquidação"
-    const distribuidor = document.createElement("span")
+
+
+
+const valorDesconto = document.createElement("Span")
+    valorDesconto.innerText = "Hoje apenas: R$2100"
+    valorDesconto.classList.add("desconto")
+
+const distribuidor = document.createElement("span")
     distribuidor.innerText = produto.distribuidor
+
+let botao = document.createElement("button")
+    botao.classList.add("botaoDesconto")
+    botao.innerText = "Em liquidação"
 
     tagUl.appendChild(tagLi)
     tagLi.appendChild(nome)
     tagLi.appendChild(valor)
-    tagLi.appendChild(liquidacao)
+    tagLi.appendChild(botao)
+    tagLi.appendChild(valorDesconto)
     tagLi.appendChild(distribuidor)
 
+function toggle(elemento, classname){
+    if (elemento.classList.contains(classname)){
+        elemento.classList.remove(classname)
+    
+    } else {
+        elemento.classList.add(classname)
+    }
+}
 
+botao.addEventListener("click", (e)=> {
 
-// liquidacao.addEventListener("click", function(verificacao))
+    toggle(e.target, "novoDesconto")
+
+    
+})
 
 //   preventDefault()
