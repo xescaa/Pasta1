1eb0618bc38f6efb26cf2cde1ba579fbb39338f2
FRANCISCO ANGHINONI <102160768+xescaa@users.noreply.github.com>
2022-07-26 14:30:02 -0400

    att

---
+++ Kenzie/Arquivos/M2/sprint 1/s1-07/database.js
@@ -0,0 +1,65 @@
+// Esta é a base de dados da Livraria
+const dataBase = [
+    {
+      title: "Dom Quixote, Miguel de Cervantes, 1605",
+      inventory: 25,
+    },
+    {
+      title: "Ulisses, James Joyce, 1922",
+      inventory: 55,
+    },
+    
+    {
+      title: "A Divina Comédia, Dante Alighieri, 1321",
+      inventory: 489,
+    },
+    {
+      title: "A Divina Comédia, Dante Alighieri, 1321",
+      inventory: 489,
+    },
+    {
+      title: "1984, George Orwell, 1949",
+      inventory: 354,
+    },
+    {
+      title: "Ilíada e Odisseia, Homero, século 8 a.c.",
+      inventory: 35,
+    },
+    {
+      title: "As Viagens de Gulliver, Jonathan Swift, 1726",
+      inventory: 0,
+    },
+    {
+      title: "As Viagens de Gulliver, Jonathan Swift, 1726",
+      inventory: 0,
+    },
+    {
+      title: "O Lobo da Estepe, Herman Hesse, 1927",
+      inventory: 0,
+    },
+    {
+      title: "A Peste, Albert Camus, 1947",
+      inventory: 689,
+    },
+    {
+      title: "Admirável Mundo Novo, Aldous Huxley, 1932",
+      inventory: 3,
+    },
+    {
+      title: "Admirável Mundo Novo, Aldous Huxley, 1932",
+      inventory: 3,
+    },
+    {
+      title: "O Senhor dos Anéis, de J.R.R. Tolkien",
+      inventory: 102,
+    },
+    {
+      title: "O Guia do Mochileiro das Galáxias, de Douglas Adams ",
+      inventory: 0,
+    },
+    {
+      title: "As Crônicas de Gelo e Fogo, de George R. R. Martin",
+      inventory: 122,
+    },
+    ];
+    
